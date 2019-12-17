93cf91c5cd81c8ad8ce5387c01a9509d6f73cfa1
sreekanth9393 <sreekanthsomavarapu@gmail.com>
2019-12-17 15:16:40 +0530

    Initial commit

---
+++ for.py
@@ -0,0 +1,9 @@
+x=int(input("number"))
+for i in range(1,x):
+    for j in range(1,x-i):
+        print(' ',end='')
+    for m in range(i,0,-1):
+        print(m,end='')
+    for k in range(2,i+1):
+        print(k,end='')
+    print()
